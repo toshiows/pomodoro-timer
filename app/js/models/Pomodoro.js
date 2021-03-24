6fe0f2915e36e3b319fce020a5a2f0db2605e3cb
Toshio Watanabe <toshio_ws@hotmail.com>
2021-03-24 00:23:07 -0300

    creating pomodoro controller

---
+++ app/js/models/Pomodoro.js
@@ -0,0 +1,7 @@
+class Pomodoro {
+    constructor(atividade, tempoCiclo, tempoIntevalo) {
+        this._atividade = atividade;
+        this._tempoCiclo = tempoCiclo;
+        this._tempoIntervalo = tempoIntevalo;
+    }
+}
