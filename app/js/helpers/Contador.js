62a93ddfa4a798b837a72ec4a9c6ed1ad7451939
Toshio Watanabe <toshio_ws@hotmail.com>
2021-04-01 00:49:19 -0300

    adjusting timer view

--- app/js/helpers/Contador.js
+++ app/js/helpers/Contador.js
@@ -1,19 +1,22 @@
 class Contador {
     constructor(seletor) {
         this._elemento = document.querySelector(seletor);
-        this._contador = 15;
+        this._contador = 10;
     }
     countdown() {
-        let count = this._contador;
+        let minutos = this._contador;
         let segundos = 0;
         setInterval(function () {
             if (segundos == 0) {
                 segundos = 60;
-                count--;
+                minutos--;
+            }
+            if (minutos < 0) {
+                //doSomething();
             }
             segundos--;
             let seletor = document.querySelector("#contador");
-            seletor.innerHTML = `${count}:${segundos}`;
+            seletor.innerHTML = `${minutos < 10 ? `0${minutos}` : `${minutos}`}:${segundos < 10 ? `0${segundos}` : `${segundos}`}`;
         }, 1000);
     }
     template() {
--- app/ts/helpers/Contador.ts
+++ app/ts/helpers/Contador.ts
@@ -5,25 +5,30 @@ class Contador {
 
     constructor(seletor: string) {
         this._elemento = document.querySelector(seletor);
-        this._contador = 15;
+        this._contador = 10;
     }
 
     countdown(): void {
 
-        let count:number = this._contador;
+        let minutos:number = this._contador;
         let segundos:number = 0;
-
+        
         setInterval(function(){
-
-            if(segundos == 0){
+              
+            if(segundos == 0) {
                 segundos = 60;
-                count --;
+                minutos --;
+            }
+
+            if(minutos < 0) {
+                //doSomething();
             }
 
             segundos --;
-
+            
             let seletor = document.querySelector("#contador");
-            seletor.innerHTML = `${count}:${segundos}`;
+            seletor.innerHTML = `${minutos < 10 ? `0${ minutos }` : `${ minutos }`}:${segundos < 10 ? `0${ segundos }` : `${ segundos }`}`;
+        
         }, 1000)
     }
 
