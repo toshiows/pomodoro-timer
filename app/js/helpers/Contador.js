ddc9c8812672327b9ad31986b6704047b53012e3
Toshio Watanabe <toshio_ws@hotmail.com>
2021-04-05 18:07:01 -0300

    countdown adjusts

--- app/js/helpers/Contador.js
+++ app/js/helpers/Contador.js
@@ -1,44 +1,48 @@
 class Contador {
     constructor(seletor) {
+        this.countDownTask = (pomodoro) => {
+            let elementoPage = this._elemento;
+            let minutos = pomodoro.tempoCiclo;
+            let segundos = 0;
+            let interval = this.countDownInterval;
+            let task = setInterval(function () {
+                if (segundos == 0) {
+                    segundos = 60;
+                    minutos--;
+                }
+                if (minutos < 0) {
+                    interval(pomodoro);
+                    clearInterval(task);
+                    return;
+                }
+                segundos--;
+                elementoPage.innerHTML = `${minutos < 10 ? `0${minutos}` : `${minutos}`}:${segundos < 10 ? `0${segundos}` : `${segundos}`}`;
+            }, 1000);
+        };
+        this.countDownInterval = (pomodoro) => {
+            let elementoPage = this._elemento;
+            let minutos = pomodoro.tempoIntervalo;
+            let segundos = 0;
+            let task = setInterval(function () {
+                if (segundos == 0) {
+                    segundos = 60;
+                    minutos--;
+                }
+                if (minutos < 0) {
+                    clearInterval(task);
+                    return;
+                }
+                segundos--;
+                elementoPage.innerHTML = `${minutos < 10 ? `0${minutos}` : `${minutos}`}:${segundos < 10 ? `0${segundos}` : `${segundos}`}`;
+            }, 1000);
+        };
         this._elemento = document.querySelector(seletor);
+        this._atividade = document.querySelector('#atividade');
     }
     countdown(pomodoroList) {
         pomodoroList.toArray().forEach(pomodoro => {
             this.countDownTask(pomodoro);
-            this.countDownInterval(pomodoro);
+            //this.countDownInterval(pomodoro);
         });
-    }
-    countDownTask(pomodoro) {
-        let elementoPage = this._elemento;
-        let minutos = pomodoro.tempoCiclo;
-        let segundos = 0;
-        setInterval(function () {
-            if (segundos == 0) {
-                segundos = 60;
-                minutos--;
-            }
-            if (minutos < 0) {
-                return;
-            }
-            segundos--;
-            elementoPage.innerHTML = `${minutos < 10 ? `0${minutos}` : `${minutos}`}:${segundos < 10 ? `0${segundos}` : `${segundos}`}`;
-        }, 1000);
-    }
-    countDownInterval(pomodoro) {
-        let elementoPage = this._elemento;
-        alert("oi");
-        let minutos = pomodoro.tempoIntervalo;
-        let segundos = 0;
-        setInterval(function () {
-            if (segundos == 0) {
-                segundos = 60;
-                minutos--;
-            }
-            if (minutos < 0) {
-                return;
-            }
-            segundos--;
-            elementoPage.innerHTML = `${minutos < 10 ? `0${minutos}` : `${minutos}`}:${segundos < 10 ? `0${segundos}` : `${segundos}`}`;
-        }, 1000);
     }
 }
--- app/ts/helpers/Contador.ts
+++ app/ts/helpers/Contador.ts
@@ -1,63 +1,74 @@
 class Contador {
 
     private _elemento: Element;
+    private _atividade: Element;
 
     constructor(seletor: string) {
         this._elemento = document.querySelector(seletor);
+        this._atividade = document.querySelector('#atividade');
     }
 
     countdown(pomodoroList: PomodoroList): void {
         pomodoroList.toArray().forEach(pomodoro => {
+
             this.countDownTask(pomodoro);
-            this.countDownInterval(pomodoro);
+
+            //this.countDownInterval(pomodoro);
+
         });
     }
 
-    countDownTask(pomodoro: Pomodoro) {
+    countDownTask = (pomodoro: Pomodoro): void => {
+
         let elementoPage = this._elemento;
-            let minutos: number = pomodoro.tempoCiclo;
-            let segundos:number = 0;
+        let minutos: number = pomodoro.tempoCiclo;
+        let segundos: number = 0;
 
-                setInterval(function() {
-              
-                    if(segundos == 0) {
-                        segundos = 60;
-                        minutos --;
-                    }
+        let interval = this.countDownInterval;
+        let task = setInterval(function () {
 
-                    if(minutos < 0) {
-                        return;
-                    }
+            if (segundos == 0) {
+                segundos = 60;
+                minutos--;
+            }
 
-                    segundos --;
-        
-                    elementoPage.innerHTML = `${minutos < 10 ? `0${ minutos }` : `${ minutos }`}:${segundos < 10 ? `0${ segundos }` : `${ segundos }`}`;
-                
-                }, 1000) 
+            if (minutos < 0) {
+                interval(pomodoro);
+                clearInterval(task);
+                return;
+            }
+
+            segundos--;
+
+            elementoPage.innerHTML = `${minutos < 10 ? `0${minutos}` : `${minutos}`}:${segundos < 10 ? `0${segundos}` : `${segundos}`}`;
+
+        }, 1000)
+
     }
 
+    countDownInterval = (pomodoro: Pomodoro): void => {
 
-      countDownInterval(pomodoro: Pomodoro) {
+
         let elementoPage = this._elemento;
-        alert("oi");
-            let minutos: number = pomodoro.tempoIntervalo;
-            let segundos:number = 0;
+        let minutos: number = pomodoro.tempoIntervalo;
+        let segundos: number = 0;
 
-                setInterval(function() {
-              
-                    if(segundos == 0) {
-                        segundos = 60;
-                        minutos --;   
-                    }
+        let task = setInterval(function () {
 
-                    if(minutos < 0) {
-                        return;
-                    }
+            if (segundos == 0) {
+                segundos = 60;
+                minutos--;
+            }
 
-                    segundos --;
-        
-                    elementoPage.innerHTML = `${minutos < 10 ? `0${ minutos }` : `${ minutos }`}:${segundos < 10 ? `0${ segundos }` : `${ segundos }`}`;
-                
-                }, 1000)
+            if (minutos < 0) {
+                clearInterval(task);
+                return;
+            }
+
+            segundos--;
+
+            elementoPage.innerHTML = `${minutos < 10 ? `0${minutos}` : `${minutos}`}:${segundos < 10 ? `0${segundos}` : `${segundos}`}`;
+
+        }, 1000)
     }
 }
